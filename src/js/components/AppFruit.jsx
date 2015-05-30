11c0c41ae39bb35f7313fb821919a2194f16afd8
Claire Mitchell <claireinezmitchell@gmail.com>
2015-05-30 21:12:18 +0100

    Working search bar

--- src/js/components/AppFruit.jsx
+++ src/js/components/AppFruit.jsx
@@ -6,11 +6,22 @@ var Footer = require("./Footer.jsx");
 var AppFruit = React.createClass({
 	getInitialState: function () {
 		return {
-			fruitAdded: false
+			arrayOfFruit: [{name: "Chicken", number: 6},
+							{name: "Apples", number: 2},
+							{name: "Oranges", number: 4},
+							{name: "Peaces", number: 1}]
 		};
 	},
+	changeText: function(text) {
+		return this.setState({headerText: text});
+	},
 	render: function () {
-		<div className="fruit-list">
-			
+		return(
+			<div className="app-wrapper">
+				<Header />
+			</div>
+		);
 	}
-})
+});
+
+module.exports = AppFruit;
